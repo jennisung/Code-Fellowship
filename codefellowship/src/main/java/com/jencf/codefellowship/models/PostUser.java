d0f9494f79b4e9adf6ea94d27806cc587c90e7e7
Jennifer Sung <sung.jenni93@gmail.com>
2023-08-02 21:39:02 -0400

    fixing users(id) link

--- codefellowship/src/main/java/com/jencf/codefellowship/models/PostUser.java
+++ codefellowship/src/main/java/com/jencf/codefellowship/models/PostUser.java
@@ -1,3 +1,4 @@
+
 package com.jencf.codefellowship.models;
 
 import jakarta.persistence.*;
@@ -52,3 +53,65 @@ public class PostUser {
         this.createdAt = createdAt;
     }
 }
+
+
+//package com.jencf.codefellowship.models;
+
+//import jakarta.persistence.*;
+//import java.time.LocalDateTime;
+//
+//@Entity
+//public class PostUser {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    Long id;
+//
+//    String body;
+//
+//    LocalDateTime createdAt;
+//
+//    @ManyToOne
+//    ApplicationUser applicationUser;
+//
+//    public PostUser() {
+//    }
+//
+//    public PostUser(String body, LocalDateTime createdAt, ApplicationUser applicationUser) {
+//        this.body = body;
+//        this.createdAt = createdAt;
+//        this.applicationUser = applicationUser;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getBody() {
+//        return body;
+//    }
+//
+//    public void setBody(String body) {
+//        this.body = body;
+//    }
+//
+//    public LocalDateTime getCreatedAt() {
+//        return createdAt;
+//    }
+//
+//    public void setCreatedAt(LocalDateTime createdAt) {
+//        this.createdAt = createdAt;
+//    }
+//
+//    public ApplicationUser getApplicationUser() {
+//        return applicationUser;
+//    }
+//
+//    public void setApplicationUser(ApplicationUser applicationUser) {
+//        this.applicationUser = applicationUser;
+//    }
+//}
+
